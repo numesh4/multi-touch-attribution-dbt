2403955cac8f11343371cff8ca2dff2212043da0
numesh4 <numesh.work@gmail.com>
2026-08-12 23:46:14 +1000

    feat: add is_repeat_purchase flag to stg_conversions

--- models/staging/stg_conversions.sql
+++ models/staging/stg_conversions.sql
@@ -12,7 +12,8 @@ select
   cust.phone_number as customer_phone,
   cu.customer_profile_id as customer_profile_id,
   cu.match_method as match_method,
-  'raw_shopify.shopify_orders' as raw_source
+  'raw_shopify.shopify_orders' as raw_source,
+  row_number() over (partition by o.customer_id order by o.created_at) > 1 as is_repeat_purchase
 from {{ source('shopify','shopify_orders') }} as o
 left join {{ ref('stg_customers_unification') }} as cu
   on o.customer_id = cu.known_customer_id
